b313fe287c7091ca323a34fc8620d2887bd38f9a
brodieclarksonrabelais <brodie.clarkson2@gmail.com>
2025-11-28 14:48:29 +0100

    carousel + connexion

--- entete.php
+++ entete.php
@@ -6,6 +6,28 @@
     <title>entete</title>
 </head>
 <body>
-    
+    <div class="row">
+        <div class="col-sm-9">
+            <!--barre de recherche (include)-->
+                <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="javascript:void(0)">Biblio-Drive</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="mynavbar">
+                        <form class="d-flex">
+                            <input class="form-control me-2" type="text" placeholder="Recherche dans le catalogue (saisie du nom de l'auteur)">
+                            <button class="btn btn-primary" type="button">Rechercher</button>
+                        </form>
+                        </div>
+                    </div>
+                    </nav>
+        </div>
+        <div class="col-sm-3">
+                <!--image Moulinsart-->
+                <img src="moulinsart.jpg" alt="moulinsart" class="d-block w-100">
+        </div>
+	</div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <title>Accueil</title>
 </head>
 <body>
@@ -11,17 +15,10 @@
 	</head>
 	<body>
 	<div class="container-fluid">
-		<div class="row">
-			<div class="col-sm-9">
-					barre de recherche (include)
-			</div>
-			<div class="col-sm-3">
-					image Moulinsart
-			</div>
-		</div>
+		<?php include 'entete.php';?>
 		<div class="row">
 		   <div class="col-sm-9">
-					carroussel / résultat de la recherche / pages d'admin (ajout d'un livre)
+					Dernières acquisitions
                     <div id="demo" class="carousel slide" data-bs-ride="carousel">
 
 					<!-- Indicators/dots -->
@@ -31,29 +28,31 @@
 						<button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
 					</div>
 					<!-- The slideshow/carousel -->
-					<div class="carousel-inner">
+					<div class="carousel-inner" style="max-width: 240px;" style="max-height: 180px;">
 						<div class="carousel-item active">
-						<img src="1984.jpg" alt="1984" class="d-block w-100">
+						<img src="covers/1984.jpg" alt="1984" class="d-block w-100">
 						</div>
 						<div class="carousel-item">
-						<img src="Emma.jpg" alt="Emma" class="d-block w-100">
+						<img src="covers/Emma.jpg" alt="Emma" class="d-block w-100">
 						</div>
 						<div class="carousel-item">
-						<img src="hamlet.jpg" alt="hamlet" class="d-block w-100">
+						<img src="covers/hamlet.jpg" alt="hamlet" class="d-block w-100">
+						</div>
+							<button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+								<span class="carousel-control-prev-icon"></span>
+							</button>
+							<button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+								<span class="carousel-control-next-icon"></span>
+							</button>
 						</div>
 					</div>
 
 					<!-- Left and right controls/icons -->
-					<button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
-						<span class="carousel-control-prev-icon"></span>
-					</button>
-					<button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
-						<span class="carousel-control-next-icon"></span>
-					</button>
-					</div>
 			</div>
 			<div class="col-sm-3">
-					formulaire de connexion / profil connecté (include)
+					<!--formulaire de connexion / profil connecté (include)-->
+					<?php include 'connexion.php';?>
+					<?php include 'connexion_admin.php';?>
 			</div>
 		</div>
 	</div>
