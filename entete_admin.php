0fce77066aa31c6637be915dbbdc5b4f3ea89d99
brodieclarksonrabelais <brodie.clarkson2@gmail.com>
2026-01-16 17:15:40 +0100

    ntm raph

--- entete_admin.php
+++ entete_admin.php
@@ -7,12 +7,13 @@
             <nav  class="navbar navbar-expand-sm navbar-dark bg-dark">
                 <div class="container-fluid">
                     <img src="livre.jpg" alt="logo" width="100px" height="100px">
-                    <a class="navbar-brand" href="javascript:void(0)"> <a href="accueil_admin.php">By chatgpt</a></a>
+                    <a class="navbar-brand" href="javascript:void(0)"> <a href="accueil_admin.php">ntm Raph</a></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
                     <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="mynavbar">
                     <form class="d-flex" action="lister_livre.php" method="get">
+
                         <input class="form-control me-2" type="text" placeholder="Recherche dans le catalogue (saisie du nom de l'auteur)" name="navbar">
                         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
                     </form>
